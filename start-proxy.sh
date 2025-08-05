650104430e0244929c7f20a37b46a99af4b21ce6
Jsweet <jsweet@sd-65385.dedibox.fr>
2025-08-05 12:03:14 +0200

    add start script

---
+++ start-proxy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Load NVM (if not already available in your shell)
+export NVM_DIR="$HOME/.nvm"
+# Source nvm.sh to use nvm command
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Use Node.js version 16
+nvm use 16
+
+# Start the app with nohup and redirect output
+nohup npm start > proxy.log 2>&1 &
+
